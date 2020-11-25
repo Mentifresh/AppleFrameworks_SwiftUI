a827a38781ff909f3fcf999739e9f84961ba4f2a
Daniel Kilders <dankil.dev@gmail.com>
2020-11-25 17:20:56 +0100

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Dani Kilders on 25.11.20.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
